b12e7944c5622d607466538d0a91d180bb41e442
Jonathan Duarte <jonathan.duarte@synergy-gb.com>
2016-05-19 11:26:26 -0400

    Primer Commit

---
+++ src/sgb-screen-loginKiraso.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('sgb-screen-loginKiraso', ['megazord'])
+    .controller('sgb-screen-loginKiraso-controller', ['$scope', '_router', '_screen', '_screenParams', '$ionicPopup',function ($scope, _router, _screen, _screenParams,$ionicPopup) {
+        _screen.initialize($scope, _screenParams);
+
+        $scope.params = _screenParams;
+
+        $scope.clearFields= function(){
+            $scope.login = {
+                usename : "",
+                password: ""
+            }
+        }
+
+        $scope.$on('$ionicView.beforeEnter', function(){
+            $scope.clearFields()
+        })
+
+        $scope.goTo = function(){
+            if(($scope.login.username != $scope.params.usernameValidation) ||
+                ($scope.login.password != $scope.params.passwordValidation)){
+                    var alerPopupPromise = $ionicPopup.alert({
+                        title: 'Usuario o Contraseña Invalida',
+                        template: 'Al tercer intento errado su clave sera bloqueada',
+                        okText: 'Cerrar' && $scope.clearFields(),
+                        okType: 'mybutton',
+                        cssClass:'animated'
+                    })
+                }
+                else{
+                    _router.fireEvent({
+                        name: 'goTo',
+                    });
+                }
+        }
+
+    }]);
